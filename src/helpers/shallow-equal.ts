8c5d88f47f3a2c8183c29e35d23c92fbe151d933
Richard <richerd.rdc@gmail.com>
2021-09-06 02:47:07 -0700

    Simplify `shallowEqual` with @rbxts/object-utils

--- src/helpers/shallow-equal.ts
+++ src/helpers/shallow-equal.ts
@@ -1,33 +1,18 @@
-function objectKeys<T extends object>(obj: T): LuaTuple<[keys: Array<keyof T>, count: number]> {
-	const keys = new Array<keyof T>();
-	let count = 0;
-	for (const [key] of pairs(obj)) {
-		keys.push(key as never);
-		count++;
-	}
-	return [keys, count] as LuaTuple<[Array<keyof T>, number]>;
-}
+import Object from "@rbxts/object-utils";
 
 /**
  * Compares two arbitrary values for shallow equality. Object values are compared based on their keys, i.e. they must
- * have the same keys and for each key the value must be equal according to the `Object.is()` algorithm. Non-object
- * values are also compared with the same algorithm as `Object.is()`.
+ * have the same keys and for each key the value must be equal.
  */
 export function shallowEqual<T>(left: T, right: unknown): boolean {
 	if (left === right) return true;
 
 	if (!typeIs(left, "table") || !typeIs(right, "table")) return false;
 
-	const [keysLeft, keysLeftSize] = objectKeys(left);
-	const [keysRight, keysRightSize] = objectKeys(right);
+	const keysLeft = Object.keys(left) as (keyof T)[];
+	const keysRight = Object.keys(right) as (keyof typeof right)[];
 
-	if (keysLeftSize !== keysRightSize) return false;
+	if (keysLeft.size() !== keysRight.size()) return false;
 
-	for (let i = 0; i < keysLeftSize; i++) {
-		if (left[keysLeft[i]] !== right[keysRight[i]]) {
-			return false;
-		}
-	}
-
-	return true;
+	return keysLeft.every((value, index) => value === right[index as never]);
 }
