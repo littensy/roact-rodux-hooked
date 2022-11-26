2f25610727cfe4244a7e862cba08f13809bcb37a
Richard <richerd.rdc@gmail.com>
2022-11-26 11:54:23 -0800

    Try new node_modules first

--- src/vendor/Roact.lua
+++ src/vendor/Roact.lua
@@ -1,9 +1,9 @@
 local modules = script:FindFirstAncestor("node_modules")
 
-if modules:FindFirstChild("roact") then
-	return require(modules.roact.src)
-elseif modules:FindFirstChild("@rbxts") then
+if modules:FindFirstChild("@rbxts") then
 	return require(modules["@rbxts"].roact.src)
+elseif modules:FindFirstChild("roact") then
+	return require(modules.roact.src)
 elseif script.Parent.Parent.Parent:FindFirstChild("Roact") then
 	return require(script.Parent.Parent.Parent.Roact)
 else
--- src/vendor/RoactHooked.lua
+++ src/vendor/RoactHooked.lua
@@ -1,9 +1,9 @@
 local modules = script:FindFirstAncestor("node_modules")
 
-if modules:FindFirstChild("roact-hooked") then
-	return require(modules["roact-hooked"].src)
-elseif modules:FindFirstChild("@rbxts") then
+if modules:FindFirstChild("@rbxts") then
 	return require(modules["@rbxts"]["roact-hooked"].src)
+elseif modules:FindFirstChild("roact-hooked") then
+	return require(modules["roact-hooked"].src)
 elseif script.Parent.Parent.Parent:FindFirstChild("roact-hooked") then
 	return require(script.Parent.Parent.Parent["roact-hooked"])
 else
